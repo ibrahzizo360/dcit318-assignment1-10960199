8dddc6b448eb214a4f14a9d97639f9c2501d7052
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-06-02 21:13:55 +0000

    done with all tasks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the GradeCalculator class
+            GradeCalculator gradeCalculator = new GradeCalculator();
+
+            float grade = gradeCalculator.GetUserGrade();
+
+           
+            string letterGrade = gradeCalculator.GetLetterGrade(grade);
+
+           
+            gradeCalculator.DisplayLetterGrade(letterGrade);
+
+
+            // Create an instance of the TicketPriceCalculator class
+            TicketPriceCalculator ticketPriceCalculator = new TicketPriceCalculator();
+
+         
+            int age = ticketPriceCalculator.GetUserAge();
+
+         
+            decimal price = ticketPriceCalculator.CalculateTicketPrice(age);
+
+            
+            ticketPriceCalculator.DisplayTicketPrice(price);
+
+
+            // Create an instance of the TriangleTypeIdentifier class
+            TriangleTypeIdentifier triangleTypeIdentifier = new TriangleTypeIdentifier();
+
+          
+            var (side1, side2, side3) = triangleTypeIdentifier.GetTriangleSidesFromUser();
+
+           
+            string triangleType = triangleTypeIdentifier.DetermineTriangleType(side1, side2, side3);
+
+           
+            triangleTypeIdentifier.DisplayTriangleType(triangleType);
+        }
+    }
+}
